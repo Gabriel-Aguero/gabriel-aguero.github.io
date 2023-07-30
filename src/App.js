f6395ce8c2815b47be7fcf70e7b2176b86ebb83d
Gabriel Aguero <gabrielhaguero@gmail.com>
2023-07-30 18:47:13 -0300

    cargando sitio en connstruccio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import pc from "./pc.svg";
+import wave from "./wave.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-Title">
+          <h1>Sitio Web en Construcción</h1>
+          <span>
+            Hola mi nombre es Gabriel Agüero soy programador Front-end. En estos
+            momentos estoy trabajando en una mejora del sitio web para brindarme
+            una mejor experiencia durante tu estadio en el sitio !!!
+          </span>
+        </div>
+        <img src={pc} className="App-logo" alt="logo" />
       </header>
+      <div className="App-footer">
+        <img src={wave} className="wave" alt="wave" />
+      </div>
     </div>
   );
 }
