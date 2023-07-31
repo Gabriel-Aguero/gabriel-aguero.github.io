1de0958a3983338a9dfc4d6c283bca17b7794cab
Gabriel Aguero <gabrielhaguero@gmail.com>
2023-07-31 01:21:13 -0300

    estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import pc from "./pc.svg";
-import wave from "./wave.svg";
+import my from "./programer.png";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-Title">
-          <h1>Sitio Web en Construcción</h1>
-          <span>
-            Hola mi nombre es Gabriel Agüero soy programador Front-end. En estos
-            momentos estoy trabajando en una mejora del sitio web para brindarme
-            una mejor experiencia durante tu estadio en el sitio !!!
+    <div className="container mx-auto px-2 lg:w-1/2 padre">
+      <div className="grid grid-cols-1 gap-4">
+        <div className="mt-20 flex flex-col gap-5 text-center">
+          <h1 className="text-3xl font-bold">Sitio Web en Construcción 👷‍♂️</h1>
+          <span className="text-xl font-mono">
+            🖐 Hola mi nombre es Gabriel Agüero soy programador Front-end 👨‍💻. En
+            estos momentos estoy trabajando en mejorar el sitio web para
+            brindarte una mejor experiencia durante tu estadio en el sitio !!!
+            Pronto va a Pronto va a estar disponible !!!
           </span>
         </div>
-        <img src={pc} className="App-logo" alt="logo" />
-      </header>
-      <div className="App-footer">
-        <img src={wave} className="wave" alt="wave" />
+
+        <div className="flex justify-center rounded-2xl border-1 py-5 border-slate-600 shadow-2xl shadow-slate-600 mt-10">
+          <img src={my} className="App-logo" alt="logo" />
+        </div>
       </div>
     </div>
   );
